8778e9bfc6a5e4ab5cbd129913d1b3ae51cbb8d4
ma-foyez <mafoyez.bd@gmail.com>
2022-10-11 17:34:48 +0600

    add validation

--- src/screens/protected/Create.jsx
+++ src/screens/protected/Create.jsx
@@ -7,6 +7,7 @@ import RadioInput from '../../components/common/RadioInput'
 import Button from '../../theme/Button'
 import { addDoc, collection } from 'firebase/firestore'
 import { db } from '../../config/firebaseConfig'
+import { FlashMessage } from '../../components/common/FlashMessage'
 
 export default function Create({ user, navigation, route }) {
 
@@ -14,6 +15,9 @@ export default function Create({ user, navigation, route }) {
     { label: "Red", value: "#de0f04" },
     { label: "Blue", value: "#007a8c" },
     { label: "Green", value: "#06b864" },
+    { label: "Orange", value: "#ff6a19" },
+    { label: "Light Red", value: "#f0055f" },
+    { label: "Violet", value: "#7f0ee3" },
   ];
   const [loading, setLoading] = useState(false)
   const [note, setNote] = useState({
@@ -23,6 +27,19 @@ export default function Create({ user, navigation, route }) {
   })
 
   const handleSubmitNote = async () => {
+
+    if(note.title === ""){
+      FlashMessage("Title can't be blank!", "danger");
+      return false;
+    }
+    if(note.description === ""){
+      FlashMessage("Description can't be blank!", "danger");
+      return false;
+    }
+    if(note.theme === ""){
+      FlashMessage("Please select theme...", "danger");
+      return false;
+    }
     let collectionRef = collection(db, "notes");
     setLoading(true);
     try {
@@ -33,6 +50,8 @@ export default function Create({ user, navigation, route }) {
         theme: note.theme,
       })
       setLoading(false);
+      navigation.goBack();
+      FlashMessage("New note created successfully...", "success");
     } catch (err) {
       console.log('err', err);
       setLoading(false);
--- src/screens/protected/List.jsx
+++ src/screens/protected/List.jsx
@@ -5,6 +5,7 @@ import Text from '../../theme/Text';
 import { AntDesign } from '@expo/vector-icons';
 import { collection, onSnapshot, query, where, getDocs } from 'firebase/firestore';
 import { db } from '../../config/firebaseConfig';
+import { spacing } from './../../theme/spacing';
 
 export default function List({ user, navigation, route }) {
   // console.log('user :>> ', user);
@@ -24,38 +25,47 @@ export default function List({ user, navigation, route }) {
   }, [])
 
   const renderItem = ({ item }) => {
+    const { title, description, theme } = item;
+
     return (
-      <Pressable>
-        <Text>{item.title}</Text>
+      <Pressable style={{ backgroundColor: theme, marginBottom: 10, borderRadius: 10, padding: 15 }}>
+        <Text preset='h3' style={{ color: "white" }}>{item.title}</Text>
+        <Text style={{ color: "white", fontSize: 16, marginTop: 10 }}>{description}</Text>
       </Pressable>
     )
   }
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.flexBetween}>
-        <Text preset='h4'>My Notes</Text>
-        <Pressable onPress={() => navigation.navigate("Create")}>
-          <AntDesign name="pluscircle" size={24} color="black" />
-        </Pressable>
-      </View>
+      <ScrollView>
+        <View style={styles.flexBetween}>
+          <Text preset='h4'>My Notes</Text>
+          <Pressable onPress={() => navigation.navigate("Create")}>
+            <AntDesign name="pluscircle" size={24} color="black" />
+          </Pressable>
+        </View>
 
-      <FlatList
-        data={notes}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.uid}
-      />
+        <View>
+          <FlatList
+            data={notes}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.title}
+            contentContainerStyle={{ padding: spacing[5] }}
+          />
 
+        </View>
+      </ScrollView>
     </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    padding: 15
+    // flex: 1,
+    // padding: 15
   },
   flexBetween: {
     flexDirection: 'row',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
+    padding: spacing[5]
   }
 })
